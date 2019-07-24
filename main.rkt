2f16bdcf44ba3193c226a1d312b6d164a39e3518
Jonathon <jyallop21@gmail.com>
2019-07-23 21:54:03 -0400

    adds some server and file io code

--- main.rkt
+++ main.rkt
@@ -1,3 +1,20 @@
 #lang racket
 
-(print "hello-world\n")
+(require web-server/servlet)
+(require web-server/servlet-env)
+(require web-server/http/request-structs)
+
+(define (read-list #:number n)
+  (if (= n 0) '() (cons (read) (read-list #:number (- n 1)))))
+
+(define output (open-output-file "resources/ouput.txt" #:exists 'replace))
+
+
+(define (start req)
+  (write req output) 
+  (response/xexpr
+    `(html (head (title "Hello, World!"))
+          (body (p "Hello, person!")))))
+
+(serve/servlet start)
+(close-output-port output)
